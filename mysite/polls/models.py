2ef117c7ab54cfec24ad4eb3abe823e7f47c49e3
Nupur <nupur.dichwalkar@gmail.com>
2015-09-03 15:39:31 -0400

    first commit

---
+++ mysite/polls/models.py
@@ -0,0 +1,19 @@
+# Create your models here.
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class MyChoice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=10)
